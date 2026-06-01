ffc5a7146bb865bf0c36d5221b450483328855c6
Vidya Mavuri <vidyamav25@gmail.com>
2026-06-01 11:15:10 -0700

    Add HomePage class for Amazon search functionality

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,1 +1,13 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
+class HomePage(BasePage):
+    SEARCH_BOX = (By.ID, "twotabsearchtextbox")
+    SEARCH_BUTTON = (By.ID, "nav-search-submit-button")
+
+    def open(self):
+        self.driver.get("https://www.amazon.com")
+
+    def search(self, query):
+        self.type(self.SEARCH_BOX, query)
+        self.click(self.SEARCH_BUTTON)
