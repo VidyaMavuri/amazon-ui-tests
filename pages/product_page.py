76a6614ae55031fa72c9d3f830bcb4d1f459b0bb
Vidya Mavuri <vidyamav25@gmail.com>
2026-06-01 11:15:47 -0700

    Implement add to cart functionality in ProductPage

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,1 +1,12 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
+class ProductPage(BasePage):
+    ADD_TO_CART = (By.ID, "add-to-cart-button")
+    CART_CONFIRMATION = (By.CSS_SELECTOR, "#sw-atc-details-single-container")
+
+    def add_to_cart(self):
+        self.click(self.ADD_TO_CART)
+
+    def is_added_to_cart(self):
+        return self.wait_for(self.CART_CONFIRMATION)
