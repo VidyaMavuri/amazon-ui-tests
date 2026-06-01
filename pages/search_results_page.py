c1edeeabfcc6b6f8b5eb1e407f0d753a5962c823
Vidya Mavuri <vidyamav25@gmail.com>
2026-06-01 11:15:31 -0700

    Add SearchResultsPage class with product click method

--- pages/search_results_page.py
+++ pages/search_results_page.py
@@ -1,1 +1,8 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
+class SearchResultsPage(BasePage):
+    FIRST_PRODUCT = (By.CSS_SELECTOR, "div[data-component-type='s-search-result'] h2 a")
+
+    def click_first_product(self):
+        self.click(self.FIRST_PRODUCT)
