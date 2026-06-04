386669795f703f85b69ac119476d93838db9ad53
Vidya Mavuri <vidyamav25@gmail.com>
2026-06-04 08:43:00 -0700

    Add test for Amazon search functionality
    Added a test for searching on Amazon using the HomePage and SearchResultsPage classes.

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,1 +1,18 @@
+from utils.driver_factory import get_driver
+from pages.home_page import HomePage
+from pages.search_results_page import SearchResultsPage
 
+def test_search_amazon():
+    driver = get_driver()
+    try:
+        home = HomePage(driver)
+        results = SearchResultsPage(driver)
+
+        home.open()
+        home.search("wireless mouse")
+
+        results.wait_for(results.FIRST_PRODUCT)
+        assert True, "Search results loaded successfully"
+
+    finally:
+        driver.quit()
